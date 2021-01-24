f46c4dafb969d83b264b5115bdcff158e8100d08
i.yaskin <i.yaskin@globus-ltd.com>
2021-01-24 18:21:15 +0300

    Quest view

--- FreetimeQuest/ViewController.swift
+++ FreetimeQuest/ViewController.swift
@@ -11,16 +11,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var questBgView: UIView!
+    @IBOutlet weak var questTitleLabel: UILabel!
+    @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var doneQuestButton: UIButton!
+    @IBOutlet weak var changeQuestButton: UIButton!
+    @IBOutlet weak var deleteQuestButton: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         titleLabel.text = "Freetime Quest"
-        titleLabel.textAlignment = .center
-        titleLabel.font = .systemFont(ofSize: 40, weight: .medium)
+        questTitleLabel.text = "Посетить музей"
+        doneQuestButton.setTitle("Готово", for: .normal)
+        changeQuestButton.setTitle("Поменять", for: .normal)
+        deleteQuestButton.setTitle("Удалить", for: .normal)
+        
     }
 
-
+    @IBAction func doneQuestTapped(_ sender: UIButton) {
+    }
+    @IBAction func changeQuestTapped(_ sender: UIButton) {
+    }
+    @IBAction func deleteQuestTapped(_ sender: UIButton) {
+    }
+    
 }
 
