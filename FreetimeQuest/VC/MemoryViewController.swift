7faaef8286415e654131359848146839e8800bd8
i.yaskin <i.yaskin@globus-ltd.com>
2021-03-14 15:58:10 +0300

    Add MemoryVC

---
+++ FreetimeQuest/VC/MemoryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MemoryViewController.swift
+//  FreetimeQuest
+//
+//  Created by Igor Yaskin on 14.03.2021.
+//
+
+import UIKit
+
+class MemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+    }
+}
