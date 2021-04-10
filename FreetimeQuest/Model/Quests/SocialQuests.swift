3054979ce9f7d1ef9d8ec4da73cfb40fc046c7c4
i.yaskin <i.yaskin@globus-ltd.com>
2021-04-10 09:52:37 +0300

    getIcon for social

--- FreetimeQuest/Model/Quests/SocialQuests.swift
+++ FreetimeQuest/Model/Quests/SocialQuests.swift
@@ -130,8 +130,50 @@ class SocialQuests {
     static func getIcon(id: Int) -> UIImage? {
         switch id {
         case 1:
-            return UIImage(named: "icon")
-
+            return UIImage(named: "smile")
+        case 2:
+            return UIImage(named: "blink")
+        case 3:
+            return UIImage(named: "uglyface")
+        case 4:
+            return UIImage(named: "10questions")
+        case 5:
+            return UIImage(named: "anekdot")
+        case 6:
+            return UIImage(named: "socialphoto")
+        case 7:
+            return UIImage(named: "selfi")
+        case 8:
+            return UIImage(named: "compliment")
+        case 9:
+            return UIImage(named: "sayhello")
+        case 10:
+            return UIImage(named: "oboznalsya")
+            
+        case 11:
+            return UIImage(named: "exchangenumbers")
+        case 12:
+            return UIImage(named: "loveletter")
+        case 13:
+            return UIImage(named: "tellpoem")
+        case 14:
+            return UIImage(named: "sing")
+        case 15:
+            return UIImage(named: "meetsomeone")
+        case 16:
+            return UIImage(named: "askforsalt")
+        case 17:
+            return UIImage(named: "wishGoodTrip")
+        case 18:
+            return UIImage(named: "swimInFountain")
+        case 19:
+            return UIImage(named: "danceOnStreet")
+        case 20:
+            return UIImage(named: "makeParty")
+        case 21:
+            return UIImage(named: "makePicnic")
+        case 22:
+            return UIImage(named: "stage")
         default:
             return UIImage(named: "icon")
         }
